caf1c1830481862b352eee157639c8a75ca46263
Ycx-Company <974577817@qq.com>
2023-03-30 10:35:44 +0800

    2

--- src/main/kotlin/S3ObjectTable.kt
+++ src/main/kotlin/S3ObjectTable.kt
@@ -1,14 +1,6 @@
-import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.IntIdTable
-import org.jetbrains.exposed.sql.Column
-import org.jetbrains.exposed.sql.SqlExpressionBuilder.isNotNull
-import org.jetbrains.exposed.sql.Table
 
-object S3ObjectTable : Table() {
-
-    private val id: Column<EntityID<Int>> = integer("id").autoIncrement().entityId()
-
-    override val primaryKey = PrimaryKey(id)
+object S3ObjectTable : IntIdTable() {
 
     val s3Key = varchar("s3_key", 1024).uniqueIndex()
 
